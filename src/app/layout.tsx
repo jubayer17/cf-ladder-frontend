2a2ebaae9ab98f743c3de84f0dc850365ac60413
jubayer17 <jubayer17@cse.pstu.ac.bd>
2025-11-04 21:09:25 +0600

    Add Google AdSense script to RootLayout for ad integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google AdSense Script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4944066273658213"
+          crossOrigin="anonymous"
+        />
+
         {/* Google AdSense Account */}
         <meta
           name="google-adsense-account"
